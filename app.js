6d19b6a58e35c79453ed4caa583dbd380cb9cbe2
Hosein Sbi <hosein.snd@gmail.com>
2024-12-12 20:42:54 +0330

    Update

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (user) {
         // userInfo.textContent = `Hello, ${user.first_name} `;
         alertBox.classList.add("alert-success");
-        // alertBox.innerHTML = `<strong>Hello ${user.first_name}, </strong>Welcome to CarToonTv Mini App :)`;
-        alertBox.insertAdjacentHTML('beforeend', '<strong>Hello ${user.first_name}, </strong>Welcome to CarToonTv Mini App :)');
+        alertBox.innerHTML = `<strong>Hello ${user.first_name}, </strong>Welcome to CarToonTv Mini App :)<button type="button" class="btn-close" data-bs-dismiss="alert"></button>`;
     } else {
         // userInfo.textContent = "Hello, guest!";
         alertBox.classList.add("alert-danger");
-        alertBox.innerHTML = `<strong>Error: </strong>!اطلاعات شما در تلگرام یافت نشد`;
+        alertBox.innerHTML = `<strong>Error: </strong>!اطلاعات شما در تلگرام یافت نشد<button type="button" class="btn-close" data-bs-dismiss="alert"></button>`;
 
     }
 });
