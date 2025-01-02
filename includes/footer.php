dc98f90a58d1659cb1cbcec521ead1f64d5e3eb9
Hosein-Sbi <97523181+Hosein-Sbi@users.noreply.github.com>
2025-01-02 23:08:09 +0330

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer class="container rounded-top-pill bg-dark text-center text-white mt-auto">
+  <div class="row justify-content-center">
+    <div class="col-2"></div>
+    <div class="col-8 pt-2">
+      ساخته شده با ❤️ توسط <a class="text-body" href="https://bwmta.ir/"><span class="text-black">بلک</span> <span class="text-white">وایت</span></a>
+    </div>
+    <div class="col-2 pt-1">
+      <a href="https://www.instagram.com/bwmta.ir/" role="button"><i class="bi bi-instagram text-danger"></i></a>
+      <a href="https://t.me/BlackWhiteMTA" role="button"><i class="bi bi-telegram text-primary"></i></a>
+      <div style="font-size: 8px;">ما را دنبال کنید</div>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
